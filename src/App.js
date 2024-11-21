5640dde0cff551b5940a9e6928795b9f5f0860a2
Niccolò Fulgaro <nicco.fulgaro@outlook.it>
2024-11-21 15:27:42 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import landing from "./assets/san-mich.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="">
+      {/* Div interno fisso */}
+      <div className="absolute inset-0 flex justify-center items-center">
+        <img src={landing} alt="logo" className="w-full object-cover" />
+      </div>
+
+      {/* Rettangolo header sotto l'immagine */}
+      <div className="absolute top-0 w-full bg-[#ffda55] h-32 z-[-1]"></div>
+
+      {/* Rettangolo footer sotto l'immagine */}
+      <div className="absolute bottom-0 w-full bg-[#fab31b] h-32 z-[-1]"></div>
+
+      {/* Bottoni invisibili */}
+      <div className="absolute top-3/4 left-[22%] w-[21%] h-[10%] z-10">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://maps.google.com" // Sostituisci con il link a Google Maps
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          className="block w-full h-full"
+          title="Passa a trovarci"
+        ></a>
+      </div>
+      <div className="absolute top-3/4 left-[57%] w-[21%] h-[10%] z-10">
+        <a
+          href="tel:+1234567890" // Sostituisci con il numero di telefono
+          className="block w-full h-full"
+          title="Ordinare a casa"
+        ></a>
+      </div>
     </div>
   );
 }
